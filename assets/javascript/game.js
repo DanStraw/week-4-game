2812aeb9b48b051f3db2fb5de133478b7835b366
Daniel <danstraw86@gmail.com>
2018-02-21 18:19:17 -0800

    Changed gemValueGenerator function to crystalValueCalculator. Added this to reset functions. Made replay button to appear after a game ends. Hides when clicked, triggers reset function. Created on click function for each crystal button, adds that crystals value to playerScore. Another onclick function for any crystal to compare playerScore to randNumber. Win and loss functions to increment the same.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,93 @@
 $(document).ready(function() {
 
-    var randNumber;
+    var randNumber = 0;
     var wins = 0;
     var losses = 0;
     var greenValue;
     var blueValue;
     var pinkValue;
     var purpleValue;
-    var playerScore;
+    var playerScore = 0;
 
-    //when new round starts, new random number between is selected, new values for the gem is selected, and playerscore resets to 0
+    //when new round starts, new random number between is selected, new values for the crystal is selected, and playerscore resets to 0
     function reset() {
         randNumber = Math.floor((Math.random() * 101) + 19);
         console.log(Math.max(randNumber));
+        crystalValueGenerator();
+        console.log(greenValue, blueValue, pinkValue, purpleValue);
         $('#random-number').text(randNumber);
         playerScore = 0;
         $('#score-counter').text(playerScore);
+        $("#replay-button").hide();
     }
     reset();
 
-    //ensure none of the gems have to same value in a round
+    function playAgain() {
+        $("#replay-button").show();
+        $("#replay-button").on("click", function() {
+            reset();
+        })
+    }
 
-    function gemValueGenerator() {
-        greenValue = Math.floor(Math.random() * 12 + 1);
-        blueValue = Math.floor(Math.random() * 12 + 1);
-        pinkValue = Math.floor(Math.random() * 12 + 1);
-        purpleValue = Math.floor(Math.random() * 12 + 1);
-        parseInt(greenValue);
+    //ensure none of the crystals have to same value in a round
+
+    function crystalValueGenerator() {
+        greenValue = Math.floor((Math.random() * 12) + 1);
+        blueValue = Math.floor((Math.random() * 12) + 1);
+        pinkValue = Math.floor((Math.random() * 12) + 1);
+        purpleValue = Math.floor((Math.random() * 12) + 1);
         console.log(greenValue, blueValue, pinkValue, purpleValue);
         if ((greenValue === blueValue) || (greenValue === pinkValue) || (greenValue === purpleValue) || (blueValue === pinkValue) 
         || (blueValue === purpleValue) || (pinkValue === purpleValue)) {
-            gemValueGenerator();
+            crystalValueGenerator();
         }
     }
-    gemValueGenerator();
 
-    //when player clicks on a gem, add the value to playerScore
-    $(".btn").on("click", function() {
-        playerScore = $(this).val();
-        parseInt(playerScore);
-        console.log('button clicked');
-        console.log(playerScore);
-        $("#score-counter").val(playerScore);
+    //when player clicks on a crystal, add the value to playerScore
+    $("#green-button").on("click", function() {
+        playerScore += greenValue;
+        $("#score-counter").text(playerScore);
     })
-    //player keeps click gems until player score matches or exceeds randNumber
-    function winLoss() {
-       /* function playAgain() {
-            $(".replay-button").on("click", function() {
-                reset();
-            })
-        }*/
+    $("#blue-button").on("click", function() {
+        playerScore += blueValue;
+        $("#score-counter").text(playerScore);
+    })
+    $("#pink-button").on("click", function() {
+        playerScore += pinkValue;
+        $("#score-counter").text(playerScore);
+    })
+    $("#purple-button").on("click", function() {
+        playerScore += purpleValue;
+        $("#score-counter").text(playerScore);
+    })
+
+    $(".crystal").on("click", function() {
+        
         if (playerScore === randNumber) {
-            wins++;
-            $("win-counter").text(wins);
-            playAgain();
+            win();
+            return false;
+                    
         } else if (playerScore > randNumber) {
-            losses++;
-            $("#loss-counter").text(losses);
+            loss();  
         }
+    })
+    //player keeps click crystals until player score matches or exceeds randNumber
+    function win() {
+        wins++;
+        console.log(wins);
+        $("#win-counter").text(wins);
+        playAgain();
     }
+
+    function loss() {
+        losses++;
+        console.log(losses);
+        $("#loss-counter").text(losses);
+        playAgain();
+    }
+
+    
+    
     //if playerScore matches randNumber, add 1 to wins, reset
 
     //if playerScore exceeds randNumber, add 1 to loss, reset
