a7aa1868a41385a14628120ee403a6ddfe107da1
Daniel <danstraw86@gmail.com>
2018-02-22 17:10:09 -0800

    added disabled property to crystal buttons for when game ends. That disabled property is set to false when game resets, thereby enabling them again. Set style on disabled button to match that of enabled button.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,4 @@
 $(document).ready(function() {
-
     var randNumber = 0;
     var wins = 0;
     var losses = 0;
@@ -12,9 +11,8 @@ $(document).ready(function() {
     //when new round starts, new random number between is selected, new values for the crystal is selected, and playerscore resets to 0
     function reset() {
         randNumber = Math.floor((Math.random() * 101) + 19);
-        console.log(Math.max(randNumber));
+        $(".crystal").prop("disabled", false);
         crystalValueGenerator();
-        console.log(greenValue, blueValue, pinkValue, purpleValue);
         $('#random-number').text(randNumber);
         playerScore = 0;
         $('#score-counter').text(playerScore);
@@ -28,22 +26,20 @@ $(document).ready(function() {
             reset();
         })
     }
-
-    //ensure none of the crystals have to same value in a round
-
+    
     function crystalValueGenerator() {
         greenValue = Math.floor((Math.random() * 12) + 1);
         blueValue = Math.floor((Math.random() * 12) + 1);
         pinkValue = Math.floor((Math.random() * 12) + 1);
         purpleValue = Math.floor((Math.random() * 12) + 1);
-        console.log(greenValue, blueValue, pinkValue, purpleValue);
+        //ensure none of the crystals have to same value in a round
         if ((greenValue === blueValue) || (greenValue === pinkValue) || (greenValue === purpleValue) || (blueValue === pinkValue) 
         || (blueValue === purpleValue) || (pinkValue === purpleValue)) {
             crystalValueGenerator();
         }
     }
 
-    //when player clicks on a crystal, add the value to playerScore
+    //when player clicks on a crystal, add the value to playerScore 
     $("#green-button").on("click", function() {
         playerScore += greenValue;
         $("#score-counter").text(playerScore);
@@ -61,34 +57,29 @@ $(document).ready(function() {
         $("#score-counter").text(playerScore);
     })
 
+    //player keeps click crystals until player score matches or exceeds randNumber
     $(".crystal").on("click", function() {
-        
+    
         if (playerScore === randNumber) {
             win();
-            return false;
-                    
+            $(".crystal").prop("disabled", true);        
         } else if (playerScore > randNumber) {
             loss();  
+            $(".crystal").prop("disabled", true);
         }
     })
-    //player keeps click crystals until player score matches or exceeds randNumber
+    
     function win() {
         wins++;
-        console.log(wins);
         $("#win-counter").text(wins);
         playAgain();
     }
+    //if playerScore matches randNumber, add 1 to wins, reset
 
     function loss() {
         losses++;
-        console.log(losses);
         $("#loss-counter").text(losses);
         playAgain();
     }
-
-    
-    
-    //if playerScore matches randNumber, add 1 to wins, reset
-
     //if playerScore exceeds randNumber, add 1 to loss, reset
 })
